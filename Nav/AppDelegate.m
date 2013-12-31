45bd6128722d52d0f5f819af774bc03ad9ce9998
Jeremy <jjones9527@gmail.com>
2013-12-30 21:09:35 -0500

    First level view working

--- Nav/AppDelegate.m
+++ Nav/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "FirstLevelViewController.h"
 
 @implementation AppDelegate
 
@@ -14,8 +15,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    [self.window addSubview:navController.view];
+    FirstLevelViewController *first = [[FirstLevelViewController alloc] init];
+    UINavigationController *navigation = [[UINavigationController alloc] initWithRootViewController:first];
+    self.window.rootViewController = navigation;
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 							
